705c73c2531a4eabb4cb7f65a9297dbe021db25b
Red Gogh <redgogh@vip.qq.com>
2024-09-10 15:30:40 +0800

    Add(HttpClient)：为 MultipartBody 和 QueryBuilder 添加详细注释。

--- libraries/tools/src/main/java/com/redgogh/tools/http/MultipartBody.java
+++ libraries/tools/src/main/java/com/redgogh/tools/http/MultipartBody.java
@@ -2,6 +2,28 @@ package com.redgogh.tools.http;
 
 import java.util.LinkedHashMap;
 
+/**
+ * `MultipartBody` 是一个继承自 `LinkedHashMap<String, Object>` 的类，用于构建 multipart/form-data 请求体，
+ * 主要用于处理文件上传和复杂数据提交。
+ *
+ * <p>该类存储键值对形式的请求体，其中键为字段名，值为字段值或文件对象。
+ *
+ * <p>主要功能包括：
+ * <ul>
+ *     <li>存储和管理 multipart 请求体中的各个部分。</li>
+ *     <li>支持存储文件和其他数据类型。</li>
+ * </ul>
+ *
+ * <p>使用场景：
+ * <ul>
+ *     <li>构建 multipart/form-data 请求体，特别是在需要上传文件或发送复杂数据时。</li>
+ *     <li>简化 multipart 请求体的构建过程。</li>
+ * </ul>
+ *
+ * @see LinkedHashMap
+ * @since 1.0
+ * @author RedGogh
+ */
 public class MultipartBody extends LinkedHashMap<String, Object> {
 
 }
--- libraries/tools/src/main/java/com/redgogh/tools/http/QueryBuilder.java
+++ libraries/tools/src/main/java/com/redgogh/tools/http/QueryBuilder.java
@@ -32,11 +32,35 @@ import static com.redgogh.tools.AnyObjects.atos;
 import static com.redgogh.tools.StringUtils.strwfmt;
 
 /**
- * @author RedGogh   
+ * `QueryBuilder` 是一个继承自 `LinkedHashMap<String, String>` 的类，用于构建 HTTP 请求的查询参数。
+ * 该类存储键值对形式的查询参数，并提供方法将这些参数拼接到 URL 中。
+ *
+ * <p>主要功能包括：
+ * <ul>
+ *     <li>存储和管理查询参数。</li>
+ *     <li>将查询参数格式化并拼接到指定的 URL 上。</li>
+ * </ul>
+ *
+ * <p>使用场景：
+ * <ul>
+ *     <li>动态构建 URL 查询参数以便发送 HTTP 请求。</li>
+ *     <li>简化查询参数的拼接过程，避免手动拼接字符串。</li>
+ * </ul>
+ *
+ * @see LinkedHashMap
+ * @since 1.0
+ * @author RedGogh
  */
 public class QueryBuilder extends LinkedHashMap<String, String> {
 
-    /** 参数拼接 */
+    /**
+     * #brief: 将查询参数拼接到 URL 上
+     *
+     * <p>该方法将当前 `QueryBuilder` 中的所有查询参数拼接到指定的 URL 上。
+     *
+     * @param url 要拼接查询参数的原始 URL
+     * @return 包含查询参数的完整 URL
+     */
     public String argConcatBuild(String url) {
         if (isEmpty())
             return url;
@@ -49,5 +73,4 @@ public class QueryBuilder extends LinkedHashMap<String, String> {
 
         return strwfmt("%s?%s", url, finalArguments);
     }
-
 }
