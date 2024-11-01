06831d71d242cb738afa7b5974caa4d9c01f0c50
Red Gogh <redgogh@vip.qq.com>
2024-11-01 17:16:29 +0800

    JARVIS/FEATURE: Add decode and encode for Base64

---
+++ jarvis/scripts/commands/b64dec.py
@@ -0,0 +1,48 @@
+"""
+
+    Copyright (C) 2019-2024 RedGogh All rights reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+import console
+import base64
+
+def reg(subparsers):
+    """
+    注册命令的处理函数。
+
+    该函数接收一个子解析器作为参数，用于在命令行工具中注册子命令及其相关选项。
+    通常在此函数中会定义具体的子命令及其参数，便于后续的命令解析和处理。
+
+    :param subparsers: argparse 模块创建的子解析器对象，用于添加子命令。
+    """
+    parser = subparsers.add_parser('b64dec', help='Base64解码器')
+    parser.add_argument('value', type=str, help='Base64字符串')
+    parser.add_argument('-u', '--url_safe', action='store_true', help='使用URL安全字符编码')
+
+def handle(args):
+    """
+    处理命令行参数并执行相应操作。
+
+    该函数接收解析后的命令行参数作为参数，根据参数内容执行特定的业务逻辑。
+    通常用于在命令被调用后，根据用户输入的参数执行相应的功能。
+
+    :param args: argparse 模块解析后的参数对象，包含用户输入的参数及选项。
+    """
+    original_bytes = args.value.encode('UTF-8')
+
+    if args.url_safe:
+        console.write(base64.urlsafe_b64decode(original_bytes))
+    else:
+        console.write(base64.b64decode(original_bytes))
