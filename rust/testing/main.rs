7e6c68ae101f163c13e52139c24a909effb969ed
Red Gogh <redgogh@vip.qq.com>
2025-01-16 18:42:16 +0800

    [rust] 定时滑动页面到底部并关闭进程

--- rust/testing/main.rs
+++ rust/testing/main.rs
@@ -1,7 +1,16 @@
+use std::time::Duration;
 use tokio;
 use fantoccini::{Client, ClientBuilder};
 
 const CHROME_DRIVER: &str = "http://127.0.0.1:9515";
+
+async fn is_page_bottom(client: &Client) -> bool {
+    let ret = client.execute(r#"
+        return window.scrollY + window.innerHeight >= document.body.scrollHeight;
+    "#, vec![]).await;
+
+    ret.unwrap().as_bool().unwrap()
+}
 
 async fn open_web_driver(url: &str) -> Result<Client, Box<dyn std::error::Error>>{
     let client = ClientBuilder::native().connect(CHROME_DRIVER).await?;
@@ -13,12 +22,17 @@ async fn open_web_driver(url: &str) -> Result<Client, Box<dyn std::error::Error>
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = open_web_driver("https://www.rust-lang.org").await?;
 
-    client.execute(
-        r#"
-        window.scrollBy(0, window.innerHeight);
-        "#,
-        vec![],
-    ).await?;
+    loop {
+        if is_page_bottom(&client).await {
+            break;
+        }
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        client.execute(r#"
+            window.scrollBy(1000, window.innerHeight);
+        "#, vec![], ).await?;
+    }
+
 
     Ok(())
 }
